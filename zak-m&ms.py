67c7f140ad7119bd08a90c931a703c99678217f4
Thomas Koman <99066789@mydavinci.nl>
2021-10-13 10:23:49 +0200

    now it uses a dict instead of a lsit

--- zak-m&ms.py
+++ zak-m&ms.py
@@ -2,14 +2,34 @@ import random
 import string
 alphabet = []
 string.ascii_lowercase = alphabet
-colors = ["oranje", "blauw", "groen", "bruin"]
 
-def makeMnmBag(mnmQuan:int = 1):
+def makeMnmBagList(mnmQuan:int = 1):
+    colors = ["oranje", "blauw", "groen", "bruin"]
     bag = []
     for i in range(mnmQuan):
         colorNum = random.randint(0, 3)
         bag.append(colors[colorNum])
     return bag
+
+def makeMNmBagDict(mnmQuan:int = 1):    
+    bag = {
+        "oranje" : 0,
+        "blauw" : 0,
+        "groen" : 0,
+        "bruin" : 0
+    }
+    for i in range(mnmQuan):
+        colorNum = random.randint(0, 3)
+        if colorNum == 0 :
+            bag["oranje"] += 1
+        if colorNum == 1 :
+            bag["blauw"] += 1
+        if colorNum == 2 :
+            bag["groen"] += 1
+        if colorNum == 3 :
+            bag["bruin"] += 1
+    return bag
+
 
 def start():
     quantity = str(input("Hoeveel M&M's wil je? >>"))
@@ -19,8 +39,8 @@ def start():
     if float(quantity) % 1 != 0: print("Voer een heel getal in"); start()
     else: quantity = int(quantity)
 
-    bag = makeMnmBag(quantity)
+    bag = makeMNmBagDict(quantity)
     for i in bag:
-        print(f"Deze M&M is {i}")
+        print(f"{bag[i]} M&M's zijn {i} in deze zak")
 start()
 input()
